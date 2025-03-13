5c6ff83493e41f94320d6a672619696baca76ed4
Alishba Shahzad <164113933+Alishba56@users.noreply.github.com>
2025-03-13 11:44:02 +0500

    Update useScrollAnimation.ts

--- src/app/hooks/useScrollAnimation.ts
+++ src/app/hooks/useScrollAnimation.ts
@@ -1,21 +1,17 @@
-"use client";
-
+import { useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useAnimation, type AnimationControls } from "framer-motion";
 import { useEffect } from "react";
+import { AnimationControls } from "framer-motion";
 
-export function useScrollAnimation(): [React.RefObject<HTMLDivElement>, AnimationControls] {
+export function useScrollAnimation(): [(node?: Element | null) => void, AnimationControls] {
   const controls = useAnimation();
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
+  const [ref, inView] = useInView({ threshold: 0.25 });
 
   useEffect(() => {
     if (inView) {
       controls.start("visible");
+    } else {
+      controls.start("hidden");
     }
   }, [controls, inView]);
 
-  return [ref, controls];
-}
